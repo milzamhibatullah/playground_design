23ab5c3bc790588620affdc6f41d5eb4e0338a13
Milzam Hibatullah <hmilzam@gmail.com>
2023-05-29 15:01:58 +0700

    create initial page and controller

---
+++ lib/view/auth/login.auth.view.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+import '../../controller/login.controller.dart';
+
+class LoginAuthView extends GetView<LoginController>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
