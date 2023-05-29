fa12732361ff45666ebe247567b48688e1f636d3
Milzam Hibatullah <hmilzam@gmail.com>
2023-05-29 15:18:41 +0700

    setup color

---
+++ lib/themes/app.theme.color.dart
@@ -0,0 +1,14 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppThemeColor{
+  static const Color backgroundColor = Color(0xFF810B0F);
+  static const Color secondaryColor = Color(0xffFFBB85);
+  static const Color tertiaryColor = Color(0xFFFFC702);
+
+  static const Color textPrimaryColor = Colors.white;
+  static const Color textSecondaryColor = Color(0xFFD8D5D9);
+  static const Color textActionColor = Colors.black;
+}
--- lib/view/auth/login.auth.view.dart
+++ lib/view/auth/login.auth.view.dart
@@ -1,14 +1,41 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:playground_design/themes/app.theme.color.dart';
 
 import '../../controller/login.controller.dart';
 
-class LoginAuthView extends GetView<LoginController>{
+class LoginAuthView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
+    return Scaffold(
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        padding: const EdgeInsets.symmetric(horizontal: 17.0),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [AppThemeColor.backgroundColor, Colors.black],
 
+            ///set start background gradient color(background Color)
+            begin: FractionalOffset(0.0, 0.0),
+
+            ///set end color(black color)
+            end: FractionalOffset(0.0, 1.0),
+          ),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
